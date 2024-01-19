4a8aef328dc4ee10050e663f98c75ddca8376e1e
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-01-19 22:36:18 +0200

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #install flask 2.1.0
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
-}
+package { 'Flask' :
+          ensure   => '2.1.0',
+          provider => 'pip3'
+        }
+
+# Werkzeug
+package { 'Werkzeug':
+            ensure   => '2.2.1',
+            provider => 'pip3',
+          }
