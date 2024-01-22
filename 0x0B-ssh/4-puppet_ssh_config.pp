6a140ab72ba1b27a4009cdf27acb85a4b3dcb021
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-01-22 21:14:04 +0200

    done

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# puppet config
+
+exec {'/etc/ssh/ssh_config':
+      path    => '/bin',
+      command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+}
